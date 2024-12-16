e7f202f623a6d05c9e1d98f92842aba9826a5efe
Abdulbas1d <muhammadaliyevabdulbosid@gmail.com>
2024-12-16 15:15:59 +0500

    Add axios files

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import './App.css'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import Register from './pages/Register'
+import Login from './pages/Login'
 
 function App() {
   return (
     <div>
-      App
+      <Routes>
+        <Route index element = {<Home></Home>}></Route>
+        <Route path='/register' element = {<Register></Register>}></Route>
+        <Route path='/login' element = {<Login></Login>}></Route>
+      </Routes>
     </div>
   )
 }
